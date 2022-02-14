aa3ca990feb609769b8297911c4564a3d3161a1a
Wyatt C <wcastellan@yahoo.com>
2022-02-14 15:06:46 -0500

    add multiple entry points, code split

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js',
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static', // the repot outputs to an html file in the dist folder
+        })
     ], 
     mode: 'development'
 };
